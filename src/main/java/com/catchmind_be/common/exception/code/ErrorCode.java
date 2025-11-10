bf6cece3e7de8445f55f14f74335b1f0f75dbd5b
NamMinwu <minwoo000625@naver.com>
2025-11-10 20:28:54 +0900

    CHORE : 추가된 에러 코드

--- src/main/java/com/catchmind_be/common/exception/code/ErrorCode.java
+++ src/main/java/com/catchmind_be/common/exception/code/ErrorCode.java
@@ -15,6 +15,8 @@ public enum ErrorCode implements BaseErrorCode {
   TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "400_003", "요청 파라미터 타입이 잘못되었습니다."),
   VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "400_004", "요청 값이 유효하지 않습니다."),
   INVALID_JSON_FORMAT(HttpStatus.BAD_REQUEST, "400_005", "잘못된 JSON 형식입니다."),
+  GAME_NOT_RUNNING(HttpStatus.BAD_REQUEST, "400_006", "게임이 진행 중이 아닙니다."),
+  NOT_ENOUGH_PLAYER(HttpStatus.BAD_REQUEST, "400_007", "플레이어가 충분하지 않습니다."),
 
   // 401 UNAUTHORIZED
   UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "401_001", "인증이 필요합니다."),
@@ -28,10 +30,12 @@ public enum ErrorCode implements BaseErrorCode {
   RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "404_001", "요청한 리소스를 찾을 수 없습니다."),
   ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "404_002", "방을 찾을 수 없습니다."),
   PLAYER_NOT_FOUND(HttpStatus.NOT_FOUND, "404_003", "사용자를 찾을 수 없습니다."),
+  ROUND_STATE_NOT_FOUND(HttpStatus.NOT_FOUND, "404_004", "라운드 상태를 찾을 수 없습니다."),
 
 
   // 409 CONFLICT
   DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "409_001", "이미 존재하는 리소스입니다."),
+  GAME_ALREADY_STARTED(HttpStatus.CONFLICT, "409_002", "게임이 이미 시작되었습니다."),
 
   // 500 INTERNAL SERVER ERROR
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500_001", "서버 내부 오류가 발생했습니다."),
